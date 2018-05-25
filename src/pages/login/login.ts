dadddf91a752b591cf01b5e0d16f90d75d832ab4
Jéssica Lucas <jessical.veloso@gmail.com>
2018-05-25 01:31:27 -0300

    incluindo

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+<<<<<<< HEAD
 import { NavController} from 'ionic-angular';
+=======
+import { NavController } from 'ionic-angular';
+>>>>>>> incluindo
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+<<<<<<< HEAD
 private credential: Object = {
 email:'',
 password:''
@@ -18,4 +23,22 @@ login()
 {
   console.log('Login...')
 }
+=======
+  activeMenu: string;
+  // private credential: Object = {
+  // email:'',
+  // password:''
+  // };
+  constructor(public navCtrl: NavController) {
+    this.login();
+  }
+
+  login() {
+    console.log('Login...');
+
+    //Retira a Navegação Lateral
+    // this.navCtrl.setRoot(LoginPage);
+    // this.navCtrl.popToRoot();
+  }
+>>>>>>> incluindo
 }
